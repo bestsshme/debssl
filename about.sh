38ae3ac8c674ded3e1adf94bab1e969fbfeb4d85
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-05-28 23:57:18 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN v2.0" | lolcat 
+echo -e ""
+echo -e "Debian 7 32 bit & 64 bit"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "*Jagoanssh.com"
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e ""
