53cc579e4b9809942371b4b8d7fa94fb74385a15
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-05-30 02:55:33 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -12,10 +12,6 @@ MYIP=$(wget -qO- ipv4.icanhazip.com)
 
 	echo "-- Selamat datang di Server - IP: $MYIP --"
 	echo "----#============================#----"
-	echo "    # script by: Jucky vengeance #    "
-	echo "    # Whatsapp : 083898587500    #    "
-	echo "    # Facebook : Juckyvengeance  #    "
-	echo "    #============================#    "
 	echo ""
 	cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
 	cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
@@ -32,26 +28,22 @@ MYIP=$(wget -qO- ipv4.icanhazip.com)
 	echo -e "\e[032;1mSystem uptime:\e[0m $up"
 	echo "------------------------------------------------------------------------------"
 	echo "Apa yang ingin Anda lakukan?"
-	echo -e "\e[031;1m 1\e[0m) Buat Akun SSH/OpenVPN (\e[34;1muser-add\e[0m)"
-	echo -e "\e[031;1m 2\e[0m) Generate Akun SSH/OpenVPN (\e[34;1muser-gen\e[0m)"
-	echo -e "\e[031;1m 3\e[0m) Ganti Password Akun SSH/OpenVPN (\e[34;1muser-pass\e[0m)"
-	echo -e "\e[031;1m 4\e[0m) Tambah Masa Aktif Akun SSH/OpenVPN (\e[34;1muser-renew\e[0m)"
-	echo -e "\e[031;1m 5\e[0m) Hapus Akun SSH/OpenVPN (\e[34;1muser-del\e[0m)"
-	echo -e "\e[031;1m 6\e[0m) Buat Akun PPTP VPN (\e[34;1muser-add-pptp\e[0m)"
-	echo -e "\e[031;1m 7\e[0m) Monitoring Dropbear (\e[34;1mdropmon [PORT]\e[0m)"
-	echo -e "\e[031;1m 8\e[0m) Cek Login Dropbear, OpenSSH, PPTP VPN dan OpenVPN (\e[34;1muser-login\e[0m)"
-	echo -e "\e[031;1m 9\e[0m) Kill Multi Login Manual (1-2 Login) (\e[34;1muser-limit [x]\e[0m)"
-	echo -e "\e[031;1m10\e[0m) Daftar Akun dan Expire Date (\e[34;1muser-list\e[0m)"
-	echo -e "\e[031;1m11\e[0m) Daftar Akun Aktif (\e[34;1muser-active-list\e[0m)"
-	echo -e "\e[031;1m12\e[0m) Daftar Akun Expire (\e[34;1muser-expire-list\e[0m)"
-	echo -e "\e[031;1m13\e[0m) Disable Akun Expire (\e[34;1mdisable-user-expire\e[0m)"
-	echo -e "\e[031;1m14\e[0m) Delete Akun Expire (\e[34;1mdelete-user-expire\e[0m)"
-	echo -e "\e[031;1m15\e[0m) Banned Akun (\e[34;1mbanned-user\e[0m)"
-	echo -e "\e[031;1m16\e[0m) Restart Dropbear (\e[34;1re-drop\e[0m)"
-	echo -e "\e[031;1m17\e[0m) Memory Usage (\e[34;1mps-mem\e[0m)"
-	echo -e "\e[031;1m18\e[0m) Speedtest (\e[34;1mtest-speed\e[0m)"
-	echo -e "\e[031;1m19\e[0m) Benchmark (\e[34;1mbenchmark\e[0m)"
-	echo -e "\e[031;1m20\e[0m) Reboot Server (\e[34;1mreboot\e[0m)"
+	echo -e "\e[031;1m 1\e[0m) Buat Akun SSH/OpenVPN"
+	echo -e "\e[031;1m 2\e[0m) Ganti Password Akun SSH/OpenVPN"
+	echo -e "\e[031;1m 3\e[0m) Perbarui Akun SSH/OpenVPN"
+	echo -e "\e[031;1m 4\e[0m) Hapus Akun SSH/OpenVPN"
+	echo -e "\e[031;1m 5\e[0m) Cek Login Dropbear, OpenSSH, SSL/TLS dan OpenVPN"
+	echo -e "\e[031;1m 6\e[0m) Aktifkan Kill Multi Login"
+	echo -e "\e[031;1m 7\e[0m) Daftar Akun dan Expire Date"
+	echo -e "\e[031;1m 8\e[0m) Daftar Akun Aktif"
+	echo -e "\e[031;1m 9\e[0m) Daftar Akun Expire"
+	echo -e "\e[031;1m10\e[0m) Delete Akun Expire"
+	echo -e "\e[031;1m11\e[0m) Restart Webmin"
+	echo -e "\e[031;1m12\e[0m) Restart Dropbear"
+	echo -e "\e[031;1m13\e[0m) Restart Openssh"
+	echo -e "\e[031;1m14\e[0m) Restart Squid"
+	echo -e "\e[031;1m15\e[0m) Ganti Port Squid"
+	echo -e "\e[031;1m16\e[0m) Reboot Server"
 	echo ""
 	echo -e "\e[031;1m x\e[0m) Exit"
 	echo ""
@@ -64,122 +56,99 @@ MYIP=$(wget -qO- ipv4.icanhazip.com)
 		;;
 		2)
 		clear
-		user-gen
+		read -p "Ketik user yang akan di ganti passwordnya: " uname
+		read -p "Silahkan isi passwordnya: " pass
+		echo "$uname:$pass" | chpasswd
+		echo "Mantap mbah...!!! Password $uname user berhasil sdi ganti...!!!"
 		exit
 		;;
 		3)
 		clear
-		user-pass
+		read -p "Enter username yg di perbarui: " uname
+                read -p "Aktif sampai tanggal Thn-Bln-Hr(YYYY-MM-DD): " expdate
+                renew_user
 		exit
 		;;
 		4)
 		clear
-		user-renew
+		hapus
 		exit
 		;;
 		5)
 		clear
-		hapus
+		monssh2
 		exit
 		;;
 		6)
-		clear
-		user-add-pptp
-		exit
-		;;
-		7)
-		clear
-		read -p "Isikan Port Dropbear yang akan dimonitor: " PORT
-		dropmon $PORT
-		exit
-		;;
-		8)
-		clear
-		cek
-		exit
-		;;
-		9)
 		clear
 		read -p "Isikan Maximal Login (1-2): " MULTILOGIN
 		user-limit $MULTILOGIN
 		exit
 		;;
-		10)
+		7)
 		clear
-		member
+		not_expired_users
 		exit
 		;;
-		11)
+		8)
 		clear
-		member
+		expired_users
 		exit
 		;;
-		12)
-		clear
-		user-expire-list
-		exit
-		;;
-		13)
+		9)
 		clear
 		disable-user-expire
 		exit
 		;;
-		14)
+		10)
 		clear
 		delete-user-expire
+	        echo "user kadaluarsa berhasil di  mbah...!!!"
+		exit
+		;;
+		11)
+		clear
+		service webmin restart
+	        echo "Webmin berhasil di restart mbah...!!!" 
+		exit
+		;;
+		12)
+		clear
+		service dropbear restart
+	        echo "Dropbear berhasil di restart mbah...!!!"
+		exit
+		;;
+		13)
+		clear
+		service ssh restart
+	        echo "OpenSSH berhasil di restart mbah...!!!" 
+		exit
+		;;
+		14)
+		clear
+		service squid3 restart
+	        echo "Squid sudah di restart mbah...!!!" 
 		exit
 		;;
 		15)
 		clear
-		banned-user
+		echo "Silahkan ganti port Squid3 anda lalu klik enter"
+	        echo "Isi dengan angka tidak boleh huruf !!!"
+	        read -p "Port Squid3: " -e -i 8080 PORT
+                #sed -i 's/http_port [0-9]*\nhttp_port [0-9]*/http_port $PORT1\nhttp_port $PORT2/g' /etc/squid3/squid.conf
+                sed -i "s/http_port [0-9]*/http_port $PORT/" /etc/squid3/squid.conf
+	        service squid3 restart
+                echo "Squid3 Updated Port: $PORT" 
 		exit
 		;;
 		16)
 		clear
-		service dropbear restart
-		exit
-		;;
-		17)
-		clear
-		ps-mem
-		exit
-		;;
-		18)
-		clear
-		speedtest
-		exit
-		;;
-		19)
-		clear
-		benchmark
-		exit
-		;;
-		20)
-		clear
 		reboot
+	        echo "Vps berhasil di reboot mbah...!!!" 
 		exit
-		;;
 		x)
 		clear
 		exit
 		;;
 	esac
 done
-
-cd ~/
-rm -f /root/IP
-echo -e "* menu      : menampilkan daftar perintah"
-echo -e "* usernew   : membuat akun SSH & OpenVPN"
-echo -e "* trial     : membuat akun trial"
-echo -e "* hapus     : menghapus akun SSH & OpenVPN"
-echo -e "* cek       : cek user login"
-echo -e "* member    : daftar member SSH & OpenVPN"
-echo -e "* resvis    : restart service dropbear, webmin"
-echo -e "              squid3, OpenVPN dan SSH"
-echo -e "* reboot    : reboot VPS"
-echo -e "* speedtest : speedtest VPS"
-echo -e "* info      : menampilkan informasi sistem"
-echo -e "* about     : info script auto install"
-echo -e "* exit      : keluar dari Putty/Connecbot/"
-echo -e "              JuiceSSH"
-echo -e ""
