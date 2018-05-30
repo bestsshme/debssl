f3079ce4371329765c304bf2a266954089c8cdee
bestsshme <35902661+bestsshme@users.noreply.github.com>
2018-05-31 02:57:31 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -89,9 +89,9 @@ cp client.ovpn /home/vps/public_html/
 
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/bestsshme/debssl/master/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/bestsshme/debssl/master/badvpn-udpgw64"
+  wget -O /usr/bin/badvpn-udpgw "http://vira.cf/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw
